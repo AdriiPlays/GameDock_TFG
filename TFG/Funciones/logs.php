cfa453910bf88369045c6e3a5e47d42a7a956fe0
Adrian Muro Jimenez <59072122+AdriiPlays@users.noreply.github.com>
2026-03-03 12:25:56 +0100

    Delete TFG directory

--- TFG/Funciones/logs.php
+++
@@ -1,7 +0,0 @@
-<?php
-function registrarLog($conn, $usuario, $accion) {
-    $stmt = $conn->prepare("INSERT INTO logs (usuario, accion) VALUES (?, ?)");
-    $stmt->bind_param("ss", $usuario, $accion);
-    $stmt->execute();
-    $stmt->close();
-}
